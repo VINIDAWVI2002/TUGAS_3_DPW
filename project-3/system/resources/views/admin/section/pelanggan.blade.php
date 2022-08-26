b6071322b609d4b2f00cc44e625001260dc83d02
VINIDAWVI2002 <vinidawvi@gmail.com>
2022-08-26 15:36:58 +0700

    tugas 3

---
+++ project-3/system/resources/views/admin/section/pelanggan.blade.php
@@ -0,0 +1,6 @@
+@extends('admin.base')
+@section('content')
+<h1>
+    Halaman Pelanggan
+</h1>
+@endsection
---
+++ project-3/system/resources/views/admin/section/promo.blade.php
@@ -0,0 +1,6 @@
+@extends('admin.base')
+@section('content')
+<h1>
+    Halaman Promo
+</h1>
+@endsection
